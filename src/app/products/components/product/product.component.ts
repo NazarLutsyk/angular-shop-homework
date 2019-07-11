15d35b66f2806f34d4264d0e5c54c66f5cff8ef8
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2019-07-11 15:59:12 +0300

    task1-comments

--- src/app/products/components/product/product.component.ts
+++ src/app/products/components/product/product.component.ts
@@ -1,8 +1,8 @@
-import {Component, Input} from '@angular/core';
-import {Category} from '../../models/Category';
-import {Product} from '../../models/Product';
-import {CartService} from '../../../cart/services/cart.service';
-import {ProductsService} from '../../services/products.service';
+import { Component, Input } from '@angular/core';
+import { Category } from '../../models/Category';
+import { Product } from '../../models/Product';
+import { CartService } from '../../../cart/services/cart.service';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-product',
@@ -13,17 +13,21 @@ export class ProductComponent {
   @Input() product: Product;
 
   // how to avoid it? how i can use enum on template without creating this field?
+  // Источником данных для шаблон есть свойство класса
   categorySwitch = Category;
 
-
+  // Не стоит использовать тут зависимости.
+  // Зависимости усложняют компоненты.
   constructor(
     private cartService: CartService,
-    private productsService: ProductsService,
-  ) {
-  }
+    private productsService: ProductsService
+  ) {}
 
   onBuy() {
-    this.cartService.addProduct({...this.product});
+    // Этот компонент получил данные от родителя
+    // Не стоит тут использовать сервисы, можно просто сообщить
+    // родителю, что надо сделать. В родительском компоненте уже есть одна зависимость.
+    this.cartService.addProduct({ ...this.product });
     this.productsService.removeProduct(this.product);
     console.log(this.product.name, 'was bought');
   }
